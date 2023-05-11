24508f444309d7619842b88643608a7fa929a3c3
Olufikunmi Ogunye <ogunye_kunmi@yahoo.com>
2023-05-11 14:23:33 +0100

    Entities models Address and Patient

---
+++ PatientVisitDemo.Domain/Entities/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientVisitDemo.Domain.Entities
+{
+    public class Address
+    {
+        [Key]
+        public int Address_Id { get; set; }
+
+        [Required(ErrorMessage ="House Number is a required field.")]
+        [MaxLength(4, ErrorMessage ="House Number is 4 characters long.")]
+        public string? Line_1_number_Building { get; set; }
+
+        [Required(ErrorMessage = "Street Name is a required field.")]
+        [MaxLength(100, ErrorMessage = "Street Name is 100 characters long.")]
+        public string? Street_Name { get; set; }
+
+        [Required(ErrorMessage = "Area Locality is a required field.")]
+        [MaxLength(100, ErrorMessage = "Area Locality is 100 characters long.")]
+        public string? Area_Locality { get; set; }
+
+        [Required(ErrorMessage = "City is a required field.")]
+        [MaxLength(50, ErrorMessage = "City is 50 characters long.")]
+        public string? City { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Country is 50 characters long.")]
+        public string? Country { get; set; }
+    }
+}
---
+++ PatientVisitDemo.Domain/Entities/Patient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientVisitDemo.Domain.Entities
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Address))]
+        public int Address_Id { get; set; }
+
+
+        [Required(ErrorMessage = "First Name is a required field.")]
+        [MaxLength(25, ErrorMessage = "FirstName is 25 characters long.")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is a required field.")]
+        [MaxLength(25, ErrorMessage = "LastName is 25 characters long.")]
+        public string? LastName { get; set;}
+
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        
+        [MaxLength(25, ErrorMessage = "Gender is 6 characters long.")]
+        public string? Gender { get; set; }
+
+        [MaxLength(13, ErrorMessage = "Phone is 13 characters long.")]
+        [DataType(DataType.PhoneNumber)]
+        public string? Phone { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        public DateTime Registred { get; set; } = DateTime.UtcNow;
+    }
+}
